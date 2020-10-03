ecf617d5b4f1a727c15a33d1647e2ea98b7023ba
Patrick Pelletier <code@funwithsoftware.org>
2020-10-02 22:37:25 -0700

    Add Longan Nano stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use gd32vf103xx_hal::pac;
+use gd32vf103xx_hal::prelude::*;
+use longan_nano::{lcd, lcd_pins};
+use riscv_rt::entry;
+
 use byte_slice_cast::*;
 use embedded_graphics::drawable::Drawable;
 use embedded_graphics::drawable::Pixel;
@@ -187,6 +197,7 @@ impl Fish<'_> {
     }
 }
 
+#[derive(Copy, Clone)]
 impl FishTank<'_> {
     fn new(screen_size: Size, seed: u64) -> FishTank<'static> {
         let sprite_data = SPRITE_DATA.as_slice_of::<u16>().unwrap();
@@ -278,5 +289,42 @@ impl Iterator for TankIterator<'_> {
                 }
             }
         }
+    }
+}
+
+// adapted from
+// https://github.com/riscv-rust/longan-nano/blob/master/examples/ferris.rs
+
+#[entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Configure clocks
+    let mut rcu = dp
+        .RCU
+        .configure()
+        .ext_hf_clock(8.mhz())
+        .sysclk(108.mhz())
+        .freeze();
+    let mut afio = dp.AFIO.constrain(&mut rcu);
+
+    let gpioa = dp.GPIOA.split(&mut rcu);
+    let gpiob = dp.GPIOB.split(&mut rcu);
+
+    let lcd_pins = lcd_pins!(gpioa, gpiob);
+    let mut lcd = lcd::configure(dp.SPI0, lcd_pins, &mut afio, &mut rcu);
+    let (width, height) = (lcd.size().width as i32, lcd.size().height as i32);
+
+    // Clear screen
+    Rectangle::new(Point::new(0, 0), Point::new(width - 1, height - 1))
+        .into_styled(primitive_style!(fill_color = Rgb565::new(0, 0, 0xff)))
+        .draw(&mut lcd)
+        .unwrap();
+
+    let fish_tank = FishTank::new(lcd.size(), 0x1badd00d8badbeef);
+
+    loop {
+        fish_tank.draw(&mut lcd).unwrap();
+        fish_tank.swim();
     }
 }
